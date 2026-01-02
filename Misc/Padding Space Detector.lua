d18a4bc0fb77c67537026f5ac1d74f524ac7b3d9
shakar <136544760+shakar60@users.noreply.github.com>
2026-01-02 05:52:08 +0300

    Create Padding Space Detector.lua

---
+++ Misc/Padding Space Detector.lua
@@ -0,0 +1,18 @@
+local CoreGui = game:GetService("CoreGui")
+local spacer = CoreGui.TopBarApp.TopBarApp.UnibarLeftFrame.StackedElements.LeaderboardSpacer
+
+if spacer then
+    local parent = spacer.Parent
+    local spacers = parent:GetChildren()
+    local foundFirst = false
+    
+    for i, child in ipairs(spacers) do
+        if child.Name == "LeaderboardSpacer" then
+            if not foundFirst then
+                foundFirst = true
+            else
+                child:Destroy()
+            end
+        end
+    end
+end
