0f3bc046084219c8b1d7140ae8c0e58953f8f745
shakar <136544760+shakar60@users.noreply.github.com>
2026-01-04 00:11:43 +0300

    Update and rename Tab and Section to Window.lua

--- Misc/Window.lua
+++ Misc/Window.lua
@@ -1,3 +1,46 @@
+local WindUI
+local Local = {}
+
+do
+    local ok, result = pcall(function()
+        return require("./src/Init")
+    end)
+    
+    if ok then
+        WindUI = result
+    else 
+        WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+    end
+end
+
+Local.GameInfo = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRealXORA/Roblox/refs/heads/Main/Scripts%20/Utilities%20/Fetch%20Game%20Information.luau", true))()
+Local.Window = WindUI:CreateWindow({
+    Title = "Evade",
+    Icon = "star",
+    Author = "by sh4kar60",
+    Folder = "Evade Script",
+    Size = UDim2.fromOffset(595, 485),
+    HidePanelBackground = false,
+    SideBarWidth = 200,
+    IgnoreAlerts = true,
+    Transparent = true,
+    HideSearchbar = false,
+    Topbar = {Height = 44,ButtonsType = "Default"},
+    User = {Enabled = true,},
+    OpenButton = {
+        Title = "Evade",
+        IconColor = Color3.fromRGB(255,255,255),
+        CornerRadius = UDim.new(0,18),
+        Enabled = true,
+        Draggable = true,
+        StrokeThickness = 0,
+        Color = ColorSequence.new(
+            Color3.fromRGB(150,0,0), 
+            Color3.fromRGB(0,0,0)
+        )
+    }
+})
+
 Local.Sections = {
     Main = Local.Window:Section({ Title = "Main", Opened = true, Icon = "bolt"}),
     Misc = Local.Window:Section({ Title = "Miscellaneous", Opened = true, Icon = "diamond"}),
@@ -32,3 +75,9 @@ Local.Tabs = {
     Settings = Local.Sections.Settings:Tab({ Title = "Configuration", Icon = "folder", IconColor = Color3.fromRGB(255,255,255)}),
     Discord = Local.Sections.Settings:Tab({ Title = "Discord", Icon = "layout-panel-left", IconColor = Color3.fromRGB(255,255,255)})
 }
+
+if Local.GameInfo.Name:find("Evade") and Local.GameInfo.Creator.Name == "Hexagon Development Community" then 
+    Local.Tabs.Automations:Select()
+else
+    Local.Tabs.LagSwitch:Select()
+end
